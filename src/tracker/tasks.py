07c6050d76c598d4f373e8b31b881249dda82528
Olamidun <kolapoolamidun@gmail.com>
2021-01-30 04:03:51 +0100

    removed unused imports and cleaned up views.py file

--- src/tracker/tasks.py
+++ src/tracker/tasks.py
@@ -21,7 +21,7 @@ def track_for_discount():
                 item.last_price = last_price
                 item.save()
                 requested_price = item.requested_price
-                print(data)
+                # print(data)
 
                 if last_price <= requested_price:
                     send_mail(
--- src/tracker/views.py
+++ src/tracker/views.py
@@ -1,8 +1,5 @@
-from django.shortcuts import render
-from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 from rest_framework import generics
 from tracker.models import Item
 from tracker.serializers import ItemSerializer
@@ -26,33 +23,3 @@ class SingleItemView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ItemSerializer
     queryset = Item.objects.all()
     permission_classes = (IsAuthenticated,)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def create(self, request, *args, **kwargs):
-    #     serializer = self.get_serializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     if Item.objects.filter(user=self.request.user, url=serializer.validated_data['url']).exists():
-    #         return Response({'error': 'This product is already being tracked'})
-    #     url = serializer.validated_data['url']
-    #     crawled_data = get_data_from_jumia(url)
-    #     serializer.save(user=self.request.user)
