5ad56f8cba925a62f7ec47fb6b18ddcacbcc0d54
Olamidun <kolapoolamidun@gmail.com>
2021-02-16 23:49:20 +0100

    this

--- ptracker/settings.py
+++ ptracker/settings.py
@@ -93,11 +93,12 @@ WSGI_APPLICATION = 'ptracker.wsgi.application'
 
 REST_FRAMEWORK = {
     'NON_FIELD_ERRORS_KEY': 'error',
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
     )
 }
 
